ca8e9ea707303be469bd41e138a6b3b99f9c34f1
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-02-23 19:21:51 +0100

    VotingFactory

---
+++ contracts/VotingContract.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// contract ElectionsFactory {
+//     address[] public electionContracts;
+
+//     function createElection() public {
+//         address newElection = address(msg.sender);
+//         electionContracts.push(newElection);
+//     }
+
+//     function getElectionContracts() public view returns (address[] memory) {
+//         return electionContracts;
+//     }
+// }
+contract Elections {
+
+    address owner = msg.sender;
+
+    struct Candidate{
+        uint id;
+        string name;
+        string party;
+        uint voteCount;
+    }
+   
+   Candidate [] public candidate;
+   uint candidateCount;
+    
+
+
+    mapping (address => bool) registerd;
+    mapping (address => bool) hasVoted;
+    address[]  registeredVoters;
+   
+    uint  votingTimeLeft;
+
+    uint totalCastedVotes;
+
+
+    modifier verifiedVoter(){
+        require(!hasVoted[msg.sender], "You have voted");
+        require(registerd[msg.sender] == true, 'Not a registered Voter');
+        require(votingTimeLeft > 0, 'voting time have elapsed');
+        _;
+    }
+
+    function createCandidate(string memory _name, string memory _party) public {
+        uint id = candidateCount + 1;
+        Candidate memory newCandidate = Candidate(id, _name, _party, 0 );
+        candidate.push(newCandidate);
+        candidateCount++;
+
+    }
+
+    
+
+
+    function register() public {
+      address _voter = msg.sender;
+        require(!registerd[_voter] , 'You have registered');
+        hasVoted[msg.sender] = false;
+        registerd[_voter] = true;
+        registeredVoters.push(_voter);
+      
+        votingTimeLeft =  block.timestamp + 600 ;
+        
+    }
+
+    function vote(uint _id) public verifiedVoter  returns(uint){
+        Candidate storage Votingcandidate = candidate[_id];
+
+        Votingcandidate.voteCount ++ ;
+        hasVoted[msg.sender] = true;
+        totalCastedVotes++;
+
+        return Votingcandidate.voteCount ;
+
+
+    }
+  
+    function getWinner() public view returns (string memory) {
+    uint highestVotes = 0;
+    string memory winnerName;
+    
+    for (uint i = 0; i < candidate.length; i++) {
+        if (candidate[i].voteCount > highestVotes) {
+            highestVotes = candidate[i].voteCount;
+            winnerName = candidate[i].name;
+        } else if (candidate[i].voteCount == highestVotes) {
+            winnerName = "Inconclusive"; // Handle tie
+        }
+    }
+    
+    return winnerName;
+}
+
+    function totalCastedvotes() public view returns(uint){
+        return totalCastedVotes;
+    }
+
+
+
+    function totalRegisteredVoters() public view returns (uint){
+        return registeredVoters.length;
+    }
+
+
+
+    function votingTime() public view returns(uint){
+      return votingTimeLeft - block.timestamp ;
+    }
+    
+    
+}
+
